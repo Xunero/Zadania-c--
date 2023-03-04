3eb0dbf004bc67821d183c6d009b1a9d5d572b05
Xuner <kotek2001.01@o2.pl>
2023-03-05 00:07:22 +0100

    up

--- halik-1.cpp
+++ halik-1.cpp
@@ -13,7 +13,7 @@ int factorial(int n) {
     {
         return n * factorial(n-1);
     }
-    
+    return 0;
 }
 
 int zad1() {
@@ -22,20 +22,23 @@ int zad1() {
     cout << "Podaj liczbe do silni:" << endl;
     cin >> value;
 
-    fact = factorial(value)
-    cout << "Silnia z " << value << "jest rowna" << fact << endl;
+    fact = factorial(value);
+    cout << "Silnia z " << value << " jest rowna " << fact << endl;
 
+    return 0;
 }
 
 int star(int deapth){
     int amount = deapth;
 
-    if (amount > 0)
+    while (amount > 0)
     {
-        cout << "*"
+        cout << "*";
         --amount;
     }
-    star(deapth-1);
+     
+    cout << endl;
+    if(deapth > 0) star(deapth-1);
     return 0;
 }
 
@@ -46,23 +49,58 @@ int zad2() {
 
     star(deapth);
 
+    return 0;
 }
 
 int fibonaci(int value) {
-    
+    int fi;
+    if (value < 3) return 1;
+    fi = fibonaci(value - 2) + fibonaci(value-1);
 
+    return fi;
 }
 
 int zad3() {
     int value;
-
+    int fi;
     cout << "Podaj wartosc" << endl;
     cin >> value;
 
-    fi = fibonaci(value)
+    fi = fibonaci(value);
     cout << "Wartosc: " << value << " jest rowna " << fi << endl;
     return 0;
 }
+
+int max(int array[50]) {
+    int bigger = array[0];
+    
+    for (int i = 1; i < 50; i++)
+    {
+        if (bigger < array[i])
+        {
+            bigger = array[i];
+        }
+        
+    }
+        
+    return bigger;
+}
+
+int zad4() {
+    int array[50],ran,bigger;
+
+    for (int i = 0; i < 50; i++)
+    {
+        ran = rand()%101;
+        array[i] = ran;
+    }
+    
+    bigger = max(array);
+    cout <<"Liczba najwieksza to: " << bigger << endl;
+
+    return 0;
+}
+
 int main() {
     int chose;
 
@@ -83,5 +121,7 @@ int main() {
     default:
         break;
     }
-    
+
+    if (chose < 6 && chose > 1) return 0;
+    else main();
 }
